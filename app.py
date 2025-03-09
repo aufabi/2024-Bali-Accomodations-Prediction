7671238778934e78267f50edcacf3a1426778c60
Aufa Biahdillah <89767320+aufabi@users.noreply.github.com>
2025-03-09 20:44:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,31 +33,22 @@ stars = st.number_input("Stars", min_value=0.0, max_value=5.0, step=0.1)
 users = st.number_input("Users", min_value=0, step=1)
 num_of_features = st.number_input("Number of Features", min_value=0, step=1)
 
-# Binary features (0 or 1)
-features = {
-    "Beach": st.checkbox("Beach"),
-    "Bar": st.checkbox("Bar"),
-    "Massage": st.checkbox("Massage"),
-    "Child Care": st.checkbox("Child Care"),
-    "Restaurant Show": st.checkbox("Restaurant Show"),
-    "Bike Rent": st.checkbox("Bike Rent"),
-    "Car Rent": st.checkbox("Car Rent"),
-    "Rooftop": st.checkbox("Rooftop"),
-    "Fitness": st.checkbox("Fitness"),
-    "Spa": st.checkbox("Spa"),
-    "Inclusive": st.checkbox("Inclusive"),
-    "Billyard": st.checkbox("Billyard"),
-    "Swimming Pool": st.checkbox("Swimming Pool"),
-    "Kitchen": st.checkbox("Kitchen"),
-    "Fishing": st.checkbox("Fishing"),
-}
+numerical_features = ['travel_points', 'stars', 'users', 'num_of_features']
+one_hot_columns = ['beach', 'bar', 'massage', 'child_care',	'restaurant_show', 'bike_rent', 'car_rent',	
+                   'rooftop', 'fitness', 'spa', 'inclusive', 'billyard', 'swimming_pool', 'kitchen', 'fishing']
+feature_values = [st.checkbox(f) for f in one_hot_columns]
 
-# Convert checkboxes to 0 or 1
-feature_values = [1 if features[key] else 0 for key in features]
+input_data = pd.DataFrame([[travel_points, stars, users, num_of_features] + feature_values], 
+                          columns=numerical_features + one_hot_columns)
 
-# Make Prediction
 if st.button("Predict Price"):
-    input_data = np.array([[travel_points, stars, users, num_of_features] + feature_values])
-    input_scaled = scaler.transform(input_data)  # Apply standard scaling
+    numerical_data = input_data[numerical_features]
+    other_features = input_data.drop(columns=numerical_features)
+    
+    numerical_scaled = scaler.transform(numerical_data)
+    numerical_scaled_df = pd.DataFrame(numerical_scaled, columns=numerical_features)
+
+    input_scaled = pd.concat([numerical_scaled_df, other_features], axis=1)
     prediction = model.predict(input_scaled)
+
     st.write(f"Estimated Price: Rp {prediction[0]:,.2f}")
